508f45f1ba3009a24bac5fd433b031788cb80380
fer <ferconde87@gmail.com>
2020-03-21 17:23:43 +0700

    Updates

---
+++ precache-manifest.2e19d2f62e714237f6edea17cb858549.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "132fa94e805a00c8a43683292b13a81c",
+    "url": "/ReactPortfolio/index.html"
+  },
+  {
+    "revision": "10fc75d74c04e4a6806d",
+    "url": "/ReactPortfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "70bb8d4a9fa1cbe72f00",
+    "url": "/ReactPortfolio/static/js/2.b31d492b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactPortfolio/static/js/2.b31d492b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10fc75d74c04e4a6806d",
+    "url": "/ReactPortfolio/static/js/main.656fcfa4.chunk.js"
+  },
+  {
+    "revision": "80df3ca2f21fb4cfce14",
+    "url": "/ReactPortfolio/static/js/runtime-main.bc780183.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactPortfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
